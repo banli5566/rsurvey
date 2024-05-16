1cd75bbc32c18fe0b4c43131bf4fcfedbfdc64e1
banli5566 <juiyang0701@gmail.com>
2024-05-16 12:52:28 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,6 +1,4 @@
 const urls = [
     'https://forms.gle/xWaNQkfV2TRQ73Vw9',
-    'https://forms.gle/1ygFxK8SLUUkubit9',
-    'https://forms.gle/w2y2faY5WVsBsAZR6',
     'https://forms.gle/zAYSsUuPEViZEvZG6'
 ];
